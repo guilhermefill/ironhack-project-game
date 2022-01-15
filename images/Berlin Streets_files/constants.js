7324f21a29648c15e19870e9d6c6ee4e95e65cae
guilhermefill <guilhermefill@gmail.com>
2022-01-15 15:53:45 +0100

    deleted files

--- images/Berlin Streets_files/constants.js
+++
@@ -1,9 +0,0 @@
-const heroSize = 50;
-const obstacleSize = 25;
-const heroSpeed = 20;
-const obstacleSpeed = 1;
-const pfandValue = 0.25;
-const obstacleValue =-0.5;
-let startGame = false;
-let gameOver = false;
-let gameWon = false;
